b7a9153c19c0a474bcfab4920c64e9000610a78e
rubbuemen <ruben_077@msn.com>
2020-03-07 20:16:26 +0100

    refactor(publications): Fixed the token variable
    Fixed the token variable, now is authorization instead of access_token

--- authorization/index.js
+++ authorization/index.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 exports.user = (req, res, next) => {
   try {
     // get token
-    const token = req.headers.access_token;
+    const token = req.headers.authorization;
 
     // verify token
     const user = jwt.verify(token, process.env.JWT_SECRET);
