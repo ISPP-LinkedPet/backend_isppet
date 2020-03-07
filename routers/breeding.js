d29653d7ebc7cef00787976c8dee8ec56511e937
Mcarmen <maricarmenarenas98@hotmail.es>
2020-03-07 20:32:45 +0100

    fix(): minor bugs

--- routers/breeding.js
+++ routers/breeding.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const breedingController = require('../controllers/breeding');
-const authorization = require('../authorization');
+const authorization = require('../authorization/index');
 
 router.get('/:id', authorization.all, (req, res) => breedingController.getBreeding(req, res));
 router.post('/', authorization.all, (req, res) => breedingController.createBreading(req, res));
