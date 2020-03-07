38c95f5d3e9c0399fdf33a568d63db35aff46097
José Manuel de los Reyes <josedelosreyesgarcia@gmail.com>
2020-03-07 15:28:34 +0100

    feat(breeding): Transaction and authorization during POST
    Use of transaction and authorization during the creation of breeding pubs

--- routers/breeding.js
+++ routers/breeding.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = new express.Router();
 const breedingController = require('../controllers/breeding');
+const authorization = require('../authorization');
 
 router.get('/:id', breedingController.getBreeding);
-router.post('/', breedingController.createBreading);
+router.post('/', authorization.user, breedingController.createBreading);
 
 module.exports = router;
--- services/breeding.js
+++ services/breeding.js
@@ -10,19 +10,25 @@ exports.createBreeding = async (breedingData, connection) => {
   const pubData = {
     animal_photo: breedingData.animal_photo,
     identification_photo: breedingData.identification_photo,
-    document_status: breedingData.document_status,
+    document_status: 'In revision',
     age: breedingData.age,
     genre: breedingData.genre,
     breed: breedingData.breed,
-    transaction_status: breedingData.transaction_status,
+    transaction_status: 'In progress',
     title: breedingData.title,
+    particular_id: 1, // Hay que pillarlo
+    vaccine_passport: breedingData.vaccine_passport,
   };
 
-  const publicationId = await connection('publication').insert(pubData);
-  const breedingId = await connection('breeding').insert({
+  const trx = await connection.transaction();
+
+  const publicationId = await trx('publication').insert(pubData);
+  const breedingId = await trx('breeding').insert({
     publication_id: publicationId,
     price: breedingData.price,
   });
+
+  trx.commit();
 
   return await connection('breeding')
       .join('publication', 'breeding.publication_id', '=', 'publication.id')
