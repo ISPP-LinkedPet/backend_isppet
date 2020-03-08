397c2ddf4f1af011ce55809662dbdc117caa19e4
Rubén <ruben_077@msn.com>
2020-03-08 20:27:38 +0100

    refactor(breedings): List the accepted breendings
    Now is listed only if they are "In progress"

--- services/breeding.js
+++ services/breeding.js
@@ -4,6 +4,6 @@ exports.getBreeding = async (id, connection) => {
 };
 
 exports.getBreedingsOffers = async (connection) => {
-  const breedings = await connection('breeding').join('publication', 'breeding.publication_id', '=', 'publication.id').where({'publication.document_status': 'Accepted'}).andWhere({'publication.transaction_status': null});
+  const breedings = await connection('breeding').join('publication', 'breeding.publication_id', '=', 'publication.id').where({'publication.document_status': 'Accepted'}).andWhere({'publication.transaction_status': 'In progress'});
   return breedings;
 };
