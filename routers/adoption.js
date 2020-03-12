93bde1f1d0a19725d5c499c031b1cc99a6b07b00
jesushd103 <jhd103@gmail.com>
2020-03-12 01:41:45 +0100

    feat(adoption): Update Adoption and Get Adoption
    Update Adoption and Get Adoption functionalities have been implemente

--- routers/adoption.js
+++ routers/adoption.js
@@ -9,5 +9,12 @@ router.get('/particular', authorization.particular, (req, res) =>
 router.post('/', authorization.shelter, (req, res) =>
   adoptionController.createAdoption(req, res),
 );
+router.put('/edit/:id', authorization.shelter, (req, res) =>
+  adoptionController.updateAdoption(req, res),
+);
+
+router.get('/:id', authorization.all, (req, res) =>
+  adoptionController.getAdoption(req, res),
+);
 
 module.exports = router;
