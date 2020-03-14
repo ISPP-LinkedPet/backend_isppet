ca837f59f23e008dcdf8fe7da74d6c90610b04ee
Mcarmen <maricarmenarenas98@hotmail.es>
2020-03-14 12:35:51 +0100

    fix(vet): minor error

--- routers/vet.js
+++ routers/vet.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const vetController = require('../controllers/vet');
-// const authorization = require('../authorization/index');
+const authorization = require('../authorization/index');
 
 router.get('/', authorization.all, (req, res) => vetController.getVets(req, res));
 
