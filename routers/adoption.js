4747464d4d09388123b5c11a0a15a261bca39810
Rubén <ruben_077@msn.com>
2020-03-15 18:53:55 +0100

    Fix(adoption): Added missed endpoint for pending adoptions

--- routers/adoption.js
+++ routers/adoption.js
@@ -4,6 +4,7 @@ const adoptionController = require('../controllers/adoption');
 const authorization = require('../authorization/index');
 
 router.get('/particular', authorization.particular, (req, res) => adoptionController.getParticularAdoptions(req, res));
+router.get('/pending', authorization.moderator, (req, res) => adoptionController.getPendingAdoptions(req, res));
 router.post('/', authorization.shelter, (req, res) => adoptionController.createAdoption(req, res));
 router.put('/edit/:id', authorization.shelter, (req, res) => adoptionController.updateAdoption(req, res));
 router.get('/:id', authorization.all, (req, res) => adoptionController.getAdoption(req, res));
