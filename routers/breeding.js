ca5593740a5f03c07f1b9d1f2ec87d491abdbd51
Mcarmen <maricarmenarenas98@hotmail.es>
2020-03-24 18:06:46 +0100

    fix(breeding): router eslint

--- routers/breeding.js
+++ routers/breeding.js
@@ -3,41 +3,17 @@ const router = new express.Router();
 const breedingController = require('../controllers/breeding');
 const authorization = require('../authorization/index');
 
-router.put('/edit/:id', authorization.particular, (req, res) =>
-  breedingController.editBreeding(req, res),
-);
-router.get('/pending', authorization.moderator, (req, res) =>
-  breedingController.getPendingBreedings(req, res),
-);
-router.get('/offers', authorization.particular, (req, res) =>
-  breedingController.getBreedingsOffers(req, res),
-);
-router.get('/interested', authorization.particular, (req, res) =>
-  breedingController.getMyInterestedBreedings(req, res),
-);
-router.get('/available', authorization.particular, (req, res) =>
-  breedingController.getAvailableBreedingsForParticular(req, res),
-);
-router.post('/', authorization.particular, (req, res) =>
-  breedingController.createBreading(req, res),
-);
-router.get('/:id', authorization.all, (req, res) =>
-  breedingController.getBreeding(req, res),
-);
-router.post('/interested/:id', authorization.particular, (req, res) =>
-  breedingController.imInterested(req, res),
-);
-router.put('/accept/:id', authorization.moderator, (req, res) =>
-  breedingController.acceptBreeding(req, res),
-);
-router.put('/reject/:id', authorization.moderator, (req, res) =>
-  breedingController.rejectBreeding(req, res),
-);
-router.get('/hasRequest/:id', authorization.particular, (req, res) =>
-  breedingController.breedingHasRequest(req, res),
-);
-router.put('/finish/:id', authorization.particular, (req, res) =>
-  breedingController.finishBreeding(req, res),
-);
+router.put('/edit/:id', authorization.particular, (req, res) => breedingController.editBreeding(req, res));
+router.get('/pending', authorization.moderator, (req, res) => breedingController.getPendingBreedings(req, res));
+router.get('/offers', authorization.particular, (req, res) => breedingController.getBreedingsOffers(req, res));
+router.get('/interested', authorization.particular, (req, res) => breedingController.getMyInterestedBreedings(req, res));
+router.get('/available', authorization.particular, (req, res) => breedingController.getAvailableBreedingsForParticular(req, res));
+router.post('/', authorization.particular, (req, res) => breedingController.createBreading(req, res));
+router.get('/:id', authorization.all, (req, res) => breedingController.getBreeding(req, res));
+router.post('/interested/:id', authorization.particular, (req, res) => breedingController.imInterested(req, res));
+router.put('/accept/:id', authorization.moderator, (req, res) => breedingController.acceptBreeding(req, res));
+router.put('/reject/:id', authorization.moderator, (req, res) => breedingController.rejectBreeding(req, res));
+router.get('/hasRequest/:id', authorization.particular, (req, res) => breedingController.breedingHasRequest(req, res));
+router.put('/finish/:id', authorization.particular, (req, res) => breedingController.finishBreeding(req, res));
 
 module.exports = router;
