3dfb1bd764b694b66abaca8db4c7b599f1929c7e
Mcarmen <maricarmenarenas98@hotmail.es>
2020-03-28 10:03:00 +0100

    fix(particular): routers format

--- routers/particular.js
+++ routers/particular.js
@@ -3,8 +3,6 @@ const router = new express.Router();
 const particularController = require('../controllers/particular');
 const authorization = require('../authorization/index');
 
-router.get('/:id', authorization.all, (req, res) =>
-  particularController.getParticular(req, res),
-);
+router.get('/:id', authorization.all, (req, res) => particularController.getParticular(req, res));
 
 module.exports = router;
