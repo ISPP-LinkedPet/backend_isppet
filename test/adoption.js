1bdfca9b393cead4c691795088c3e1b6b34eb2b9
Daniel Negrón <danielnf@hotmail.es>
2020-04-09 14:45:29 +0200

    feat(tests): First tests added
    Adoption listing and adoption creation tests have been added.

---
+++ test/adoption.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const request = require('supertest');
+
+const r = request('http://localhost:8080');
+
+before(function() {
+  const login = {
+    userName: 'palinaParticular',
+    password: 'hola',
+  };
+
+  r.post('/auth/login')
+      .send(login)
+      .expect(200);
+});
+
+describe('adoptions', function() {
+  describe('GET', function() {
+    it('Should return json as default data format', function(done) {
+      r.get('/adoption/available')
+          .expect('Content-Type', /json/)
+          .expect(200, done());
+    });
+  });
+  describe('POST', function() {
+    it('Should return 200 status code and adoption along with data', function(done) {
+      const adoption = {animal_photo: 'http://ecx.images-amazon.com/images/I/91DpCeCgSBL._SL1500_.jpg',
+        type: 'Dog',
+        location: 'Avda. Reina Mercedes',
+        pedigree: true,
+        birth_date: '2020-01-01',
+        genre: 'Male',
+        breed: 'Doberman',
+        name: 'Paco',
+      };
+
+      r.post('/adoption/')
+          .send(adoption)
+          .expect(200)
+          .expect('adoption.name', 'Paco', done());
+    });
+  });
+});
