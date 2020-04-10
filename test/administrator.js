4d98442370d94e80762747af77e0c42d75aa02a9
Daniel Negrón <danielnf@hotmail.es>
2020-04-10 21:56:39 +0200

    feat(testing): New added functionality
    * Added testing methods for admin.
    * Automatic deletion of saved photos after testing the methods.
    * Test command added to the package.json file which populates the database and runs the tests. Once the tests are finished, it populates the database again to make it clean.
    
    <footer>

--- test/administrator.js
+++ test/administrator.js
@@ -1,10 +1,15 @@
-'use strict';
-
 const request = require('supertest');
+const assert = require('chai').assert;
+const app = require('../index.js').app;
+const r = request(app);
+const path = require('path');
+const fs = require('fs');
 
-const r = request('http://localhost:8080');
+let loginToken = '';
+let ad1 = '';
+const allPhotos = [];
 
-before(function() {
+before(function(done) {
   const login = {
     userName: 'danielAdministrator',
     password: 'hola',
@@ -12,15 +17,82 @@ before(function() {
 
   r.post('/auth/login')
       .send(login)
-      .expect(200);
+      .end(function(err, res) {
+        loginToken = res.body.access_token;
+        done();
+      });
 });
 
-describe('administrator', function() {
-  describe('GET', function() {
+describe('administrator', function(done) {
+  describe('GET', function(done) {
     it('Should return json as the default data format, showing the banned users', function(done) {
-      r.get('/ban/list')
-          .expect('Content-Type', /json/)
-          .expect(200, done());
+      r.get('/administrator/ban/list')
+          .set('Authorization', loginToken)
+          .expect(200)
+          .end(function(err, res) {
+            assert.equal(res.body[0].user_name, 'doggyShelter');
+            done();
+          });
+    });
+
+    it('Should return json as the default data format, showing the active users', function(done) {
+      r.get('/administrator/unban/list')
+          .set('Authorization', loginToken)
+          .expect(200)
+          .end(function(err, res) {
+            assert.equal(res.body[0].user_name, 'refugioShelter');
+            done();
+          });
+    });
+
+    it('Should show the statistics', function(done) {
+      r.get('/administrator/statistics')
+          .set('Authorization', loginToken)
+          .expect(200)
+          .end(function(err, res) {
+            assert.equal(res.body[0].breedings_count, 32);
+            done();
+          });
     });
   });
+
+  describe('POST', function(done) {
+    it('Should return 200 status code and ad along with data', function(done) {
+      const ad = {ad_type: 'CPM',
+        price: 100,
+        redirect_to: 'http://www.google.com',
+        vet_id: 1,
+        active: 1,
+      };
+
+      r.post('/administrator/ad/create')
+          .field(ad)
+          .attach('top_banner', 'public/images/ads/laHuella.png')
+          .attach('lateral_banner', 'public/images/ads/laHuella.png')
+          .set('Authorization', loginToken)
+          .end(function(err, res) {
+            ad1 = res.body.ad;
+            assert.equal(ad.price, 100);
+            done();
+          });
+    });
+  });
+});
+
+after(function(done) {
+  String(ad1.top_banner).split(',').forEach((photo) => {
+    allPhotos.push(photo);
+  });
+
+  String(ad1.lateral_banner).split(',').forEach((photo) => {
+    allPhotos.push(photo);
+  });
+
+  allPhotos.forEach((photo) => {
+    fs.unlink(path.join('public', photo), (err) => {
+      // nothing to do
+    });
+  });
+
+  done();
 });
--- test/adoption.js
+++ test/adoption.js
@@ -1,10 +1,16 @@
-'use strict';
-
 const request = require('supertest');
+const assert = require('chai').assert;
+const app = require('../index.js').app;
+const r = request(app);
+const path = require('path');
+const fs = require('fs');
 
-const r = request('http://localhost:8080');
+let loginToken = '';
+let pub1 = '';
+let pub2 = '';
+const allPhotos = [];
 
-before(function() {
+before(function(done) {
   const login = {
     userName: 'palinaParticular',
     password: 'hola',
@@ -12,43 +18,54 @@ before(function() {
 
   r.post('/auth/login')
       .send(login)
-      .expect(200);
+      .end(function(err, res) {
+        loginToken = res.body.access_token;
+        done();
+      });
 });
 
-describe('adoptions', function() {
-  describe('GET', function() {
+describe('adoptions', function(done) {
+  describe('GET', function(done) {
     it('Should return json as default data format', function(done) {
       r.get('/adoption/available')
-          .expect('Content-Type', /json/)
-          .expect(200, done());
+          .set('Authorization', loginToken)
+          .expect(200)
+          .end(function(err, res) {
+            assert.equal(res.body[0].name, 'Kiwi');
+            done();
+          });
     });
   });
 
-  describe('POST', function() {
+  describe('POST', function(done) {
     it('Should return 200 status code and adoption along with data', function(done) {
-      const adoption = {animal_photo: 'http://ecx.images-amazon.com/images/I/91DpCeCgSBL._SL1500_.jpg',
-        type: 'Dog',
+      const adoption = {type: 'Dog',
         location: 'Avda. Reina Mercedes',
-        pedigree: true,
+        pedigree: 1,
         birth_date: '2020-01-01',
         genre: 'Male',
         breed: 'Doberman',
         name: 'Paco',
       };
 
-      r.post('/adoption/')
-          .send(adoption)
-          .expect(200)
-          .expect('adoption.name', 'Paco', done());
+      r.post('/adoption')
+          .field(adoption)
+          .attach('animal_photo', 'public/images/animal_photos/doberman2.jpg')
+          .attach('animal_photo', 'public/images/animal_photos/doberman.jpg')
+          .set('Authorization', loginToken)
+          .end(function(err, res) {
+            pub1 = res.body.adoption;
+            assert.equal(pub1.name, 'Paco');
+            done();
+          });
     });
   });
 
-  describe('PUT', function() {
+  describe('PUT', function(done) {
     it('Should return 200 status code and adoption along with the update', function(done) {
-      const adoption = {animal_photo: 'http://ecx.images-amazon.com/images/I/91DpCeCgSBL._SL1500_.jpg',
-        type: 'Dog',
+      const adoption = {type: 'Dog',
         location: 'Avda. Reina Mercedes',
-        pedigree: true,
+        pedigree: 1,
         birth_date: '2020-01-01',
         genre: 'Male',
         breed: 'Doberman',
@@ -56,9 +73,49 @@ describe('adoptions', function() {
       };
 
       r.put('/adoption/edit/13')
-          .send(adoption)
-          .expect(200)
-          .expect('adoption.name', 'Paco', done());
+          .field(adoption)
+          .attach('animal_photo', 'public/images/animal_photos/doberman2.jpg')
+          .attach('animal_photo', 'public/images/animal_photos/doberman.jpg')
+          .set('Authorization', loginToken)
+          .end(function(err, res) {
+            pub2 = res.body.adoption;
+            assert.equal(pub2.name, 'Paco');
+            done();
+          });
     });
   });
+});
+
+after(function(done) {
+  String(pub1.animal_photo).split(',').forEach((photo) => {
+    allPhotos.push(photo);
+  });
+
+  String(pub1.identification_photo).split(',').forEach((photo) => {
+    allPhotos.push(photo);
+  });
+
+  String(pub1.vaccine_passport).split(',').forEach((photo) => {
+    allPhotos.push(photo);
+  });
+
+  String(pub2.animal_photo).split(',').forEach((photo) => {
+    allPhotos.push(photo);
+  });
+
+  String(pub2.identification_photo).split(',').forEach((photo) => {
+    allPhotos.push(photo);
+  });
+
+  String(pub2.vaccine_passport).split(',').forEach((photo) => {
+    allPhotos.push(photo);
+  });
+
+  allPhotos.forEach((photo) => {
+    fs.unlink(path.join('public', photo), (err) => {
+      // nothing to do
+    });
+  });
+
+  done();
 });
